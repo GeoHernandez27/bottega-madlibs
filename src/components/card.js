f26cf0a272bfa8457ddbfc93a8f146b30b523ecd
GeoHernandez27 <leopardo2@gmail.com>
2019-01-19 13:36:20 -0700

    repositioned generate button, fixed content counter for labels, cleaned up all files and reformatted lines

--- src/components/card.js
+++ src/components/card.js
@@ -27,20 +27,20 @@ class Card extends Component {
 
     constructor() {
         super()
-    
+
         this.state = INITIAL_STATE;
 
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleFormSubmit = this.handleFormSubmit.bind(this);
-    }
+    } 
 
     handleInputChange(event) {
-      
         this.setState({ [event.target.name]: event.target.value })
     }
 
     handleFormSubmit(event) {
         event.preventDefault()
+
         if(this.state.contentVisible) {
             this.setState(INITIAL_STATE)
         } else {
@@ -48,21 +48,24 @@ class Card extends Component {
         }
     }
 
-    render () {
-        
+    render() {
+
         const inputData = [
             {title: 'Color', state: this.state.color, name: 'color'},
             {title: 'Plural Noun', state: this.state.pluralNoun, name: 'pluralNoun'},
             {title: 'Adjective', state: this.state.adjectiveOne, name: 'adjectiveOne'},
             {title: 'Celebrity', state: this.state.celebOne, name: 'celebOne'},
+
             {title: 'Adjective', state: this.state.adjectiveTwo, name: 'adjectiveTwo'},
             {title: 'Noun', state: this.state.nounOne, name: 'nounOne'},
             {title: 'Number', state: this.state.numberOne, name: 'numberOne'},
             {title: 'Number', state: this.state.numberTwo, name: 'numberTwo'},
+
             {title: 'Noun', state: this.state.nounTwo, name: 'nounTwo'},
             {title: 'Adjective', state: this.state.adjectiveThree, name: 'adjectiveThree'},
             {title: 'Celebrity', state: this.state.celebTwo, name: 'celebTwo'},
             {title: 'Celebrity', state: this.state.celebThree, name: 'celebThree'},
+
             {title: 'Adjective', state: this.state.adjectiveFour, name: 'adjectiveFour'},
             {title: 'Noun', state: this.state.nounThree, name: 'nounThree'},
             {title: 'Celebrity', state: this.state.celebFour, name: 'celebFour'},
@@ -71,23 +74,21 @@ class Card extends Component {
 
         return (
             <form onSubmit={this.handleFormSubmit} className="card">
-                <div className="card_inputs">
-                    {
-                         inputData.map((data, index) => {
-                            return Input( (data), this.handleInputChange, index) 
-                        })
-                    }
+                <div className="card__inputs">
+                {
+                    inputData.map((data, index) => {
+                        return Input( (data), this.handleInputChange, index) 
+                    })
+                }
                 </div>
-                
                 <button className={`card__${!this.state.contentVisible ? 'generate' : 'clear'}`} type="submit">{!this.state.contentVisible ? 'Generate Mad Lib' : 'Clear Form'}</button>
                 {
                     this.state.contentVisible ? <Content data={this.state}/> : ''
                 }
-
-            
             </form>
         )
     }
 }
+
 
 export default Card;
--- src/components/home.js
+++ src/components/home.js
@@ -6,20 +6,12 @@ import Card from './card';
 class Home extends Component {
   render() {
     return (
-      <div className='home'>
-      
-      { Header() }
-      <Card />
-       
+      <div className="home">
+        { Header() }
+        <Card />
       </div>
     );
   }
 }
-
-// things we need to fix
-// placeholder
-
-// content labels
-// generate btn space/card-height
 
 export default Home;
