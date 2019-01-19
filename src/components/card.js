0951a5ae4cd68370b22dd799b152207f74b295fd
GeoHernandez27 <leopardo2@gmail.com>
2019-01-18 19:38:55 -0700

    Added CSS styles and header skew

--- src/components/card.js
+++ src/components/card.js
@@ -73,7 +73,9 @@ class Card extends Component {
             <form onSubmit={this.handleFormSubmit} className="card">
                 <div className="card_inputs">
                     {
-                        inputData.map(data => Input( (data), this.handleInputChange))
+                         inputData.map((data, index) => {
+                            return Input( (data), this.handleInputChange, index) 
+                        })
                     }
                 </div>
                 
